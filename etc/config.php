b7fbeb0e10ba0a72e7ee51b3553f2c2f6dad4024
alexbolck <131844503+alexbolck@users.noreply.github.com>
2024-11-23 22:28:09 -0500

    3er_hito

--- etc/config.php
+++ etc/config.php
@@ -1,23 +1,33 @@
 <?php
+    define('URL_BASE',"http://sistemalogin.test/");
+    define('DB_HOST','localhost');
+    define('DB_NAME','dbsistema');
+    define('DB_USER','root');
+    define('DB_PASS','');
 
-    $_urlBase = "http://sistemalogin.test/";
-    $host = 'localhost';
-    $namedb = 'dbsistema';
-    $userdb = 'root';
-    $paswordb = '';
+    function get_path( $type, $arg1) {
+        $basePaths = [ 'base'=>URL_BASE,
+        'views'=>URL_BASE.'views/',
+        'models'=>URL_BASE.'models/',
+        'controllers'=>URL_BASE.'controllers/'];
+        return $basePaths[$type].$arg1;
+    }
 
     function get_UrlBase($arg1){
-        return $GLOBALS['_urlBase'].$arg1;
+        return get_path('base',$arg1);
     }
     function get_views($arg1){
-        return $GLOBALS['_urlBase'].'views/'.$arg1;
+        return get_path('views',$arg1);
     }
     function get_models($arg1){
-        return $GLOBALS['_urlBase'].'models/'.$arg1;
+        return get_path('models',$arg1);
     }
     function get_controllers($arg1){
-        return $GLOBALS['_urlBase'].'controllers/'.$arg1;
+        return get_path('controllers',$arg1);
     }
+    //echo 'algo';
+    //echo get_UrlBase('');
+    //echo get_models('modeloUsuario.php');
 
     //echo $_urlBase;
     //echo "<br>";
